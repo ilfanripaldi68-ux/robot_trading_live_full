a6618119e6204b8e5335269fb328e0c5ec2b767f
ilfanripaldi68-ux <ilfanripaldi68@gmail.com>
2025-09-20 21:53:12 +0700

    Create Ohlc.dart

---
+++ lib/models/Ohlc.dart
@@ -0,0 +1,9 @@
+class Ohlc {
+  final DateTime time;
+  final double open;
+  final double high;
+  final double low;
+  final double close;
+
+  Ohlc(this.time, this.open, this.high, this.low, this.close);
+}
