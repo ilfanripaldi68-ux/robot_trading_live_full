8055d03a8ff382620363923eba1d503a2ed3c836
ilfanripaldi68-ux <ilfanripaldi68@gmail.com>
2025-09-21 02:36:43 +0700

    Update ohlc.dart

--- lib/models/ohlc.dart
+++ lib/models/ohlc.dart
@@ -1,28 +1,26 @@
 class Ohlc {
-  final DateTime time;
+  final DateTime date;
   final double open;
   final double high;
   final double low;
   final double close;
-  final double volume;
 
   Ohlc({
-    required this.time,
+    required this.date,
     required this.open,
     required this.high,
     required this.low,
     required this.close,
-    required this.volume,
   });
 
-  factory Ohlc.fromJson(List<dynamic> json) {
+  // Convert Binance response ke model
+  factory Ohlc.fromBinance(List<dynamic> json) {
     return Ohlc(
-      time: DateTime.fromMillisecondsSinceEpoch(json[0]),
+      date: DateTime.fromMillisecondsSinceEpoch(json[0]),
       open: double.parse(json[1]),
       high: double.parse(json[2]),
       low: double.parse(json[3]),
       close: double.parse(json[4]),
-      volume: double.parse(json[5]),
     );
   }
 }
