e6a76fe38fde07fb611504801febd077b7402e99
ilfanripaldi68-ux <ilfanripaldi68@gmail.com>
2025-09-21 00:27:25 +0700

    Update ohlc.dart

--- lib/models/ohlc.dart
+++ lib/models/ohlc.dart
@@ -1,12 +1,12 @@
 class Ohlc {
-  final DateTime datetime;
+  final String time;
   final double open;
   final double high;
   final double low;
   final double close;
 
   Ohlc({
-    required this.datetime,
+    required this.time,
     required this.open,
     required this.high,
     required this.low,
@@ -15,7 +15,7 @@ class Ohlc {
 
   factory Ohlc.fromJson(Map<String, dynamic> json) {
     return Ohlc(
-      datetime: DateTime.parse(json['datetime']),
+      time: json['datetime'],
       open: double.parse(json['open']),
       high: double.parse(json['high']),
       low: double.parse(json['low']),
